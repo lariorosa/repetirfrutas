abb80daafe46cce5df440a01f27401dbb24bcd50
Larissa Rosa <93224886+lariorosa@users.noreply.github.com>
2021-12-19 09:49:37 -0300

    Add files via upload

---
+++ resp4_a.js
@@ -0,0 +1,26 @@
+function repetirFruta() {
+  var inFruta = document.getElementById("inFruta");
+  var inNumero = document.getElementById("inNumero");
+  var outResposta = document.getElementById("outResposta");
+
+  var fruta = inFruta.value;
+  var numero = Number(inNumero.value);
+
+  if (fruta == "" || numero == 0 || isNaN(numero)) {
+    alert("Informe os dados corretamente...");
+    inFruta.focus();
+    return
+  }
+
+  var resposta = "";
+
+  for (var i = 1; i <numero; i++) {
+    resposta = resposta + fruta + "*";
+  }
+
+  resposta = resposta + fruta;
+  outResposta.textContent = resposta;
+}
+
+var btRepetir = document.getElementById("btRepetir");
+btRepetir.addEventListener("click", repetirFruta);
